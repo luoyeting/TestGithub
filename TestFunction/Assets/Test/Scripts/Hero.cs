c372a6f917c42f998062c5b05d7b6ce036e7b63b
luoyeting0hongmoe <luoyeting@hongmoe.com>
2018-03-13 17:50:09 +0800

    unity工程创建
    开始创建了一个空unity工程

---
+++ TestFunction/Assets/Test/Scripts/Hero.cs
@@ -0,0 +1,14 @@
+﻿internal class Hero
+{
+    public Hero(float a, float attack, float c, float d)
+    {
+        this.a = a;
+        this.attack = attack;
+        this.c = c;
+        this.d = d;
+    }
+    float a;
+    public float attack;
+    float c;
+    float d;
+}
---
+++ TestFunction/Assets/Test/Scripts/Test1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Action<int> act =  this.TestCreateActionInstance();
+        act(10);
+        act(100);
+        act(1000);
+    }
+
+    private Action<int> TestCreateActionInstance()
+    {
+        int count = 0;
+        Action<int> action = delegate (int number) {
+            count += number;
+            Debug.Log(count);
+        };
+        action(1);
+        return action;
+    }
+
+    //输出结果 1 11  111  1111
+}
